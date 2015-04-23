be7256d00b24d9904fadc0de3d365120f485800b
My Name <cid@student.chalmers.se>
2015-04-23 18:54:11 +0200

    controller and plant sampled at different intervals

--- pid2/pid.c
+++ pid2/pid.c
@@ -12,7 +12,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <pthread.h>
-#include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
 
 static const double Kp = 9.;
 static const double Ki = 5;
@@ -21,6 +22,15 @@ static const double Tf = 2.;
 static const double K = 4.;
 static const double T = 3.;
 static const double h = .1;
+
+static pthread_mutex_t mx;
+
+
+static double y = 0.0;
+static double u = 0.0;
+static double r = 0.0;
+
+static bool is_running = 1;
 
 double pid(double e) {
 	static double eold_1 = 0.0;
@@ -58,59 +68,68 @@ double plant(double u) {
 
 void *process()
 {
-	for (;;)
-	{
-		sleep(100);
+	FILE *fp_out = fopen("output.txt", "w");
+
+	if (fp_out == NULL) {
+		fprintf(stderr, "Can not write to file\n");
+		exit(1);
 	}
+
+	struct timespec ts;
+	ts.tv_sec = 0;
+	ts.tv_nsec = 100000000;
+	
+	while (is_running)
+	{
+		pthread_mutex_lock(&mx);
+			y = plant(u);
+			fprintf(fp_out, "%f\n", y);
+		pthread_mutex_unlock(&mx);
+		nanosleep(&ts, NULL);
+	}
+
+	fclose(fp_out);
 	pthread_exit(NULL);
 }
 
 void *controller()
 {
-	for (;;)
-	{
-		sleep(1000);
+	FILE *fp_in = fopen("setpointvalues.txt", "r");
+
+	if (fp_in == NULL) {
+		fprintf(stderr, "Can not open file\n");
+		exit(1);
 	}
+
+	struct timespec ts;
+	ts.tv_sec = 1;
+	ts.tv_nsec = 0;
+
+	while (fscanf(fp_in, "%lf", &r) != EOF) {
+		pthread_mutex_lock(&mx);
+			u = pid(r - y);
+		pthread_mutex_unlock(&mx);
+		nanosleep(&ts, NULL);
+	}
+
+	is_running = false;
+	fclose(fp_in);
 	pthread_exit(NULL);
 }
 
 
 int main(void) {
+	pthread_mutex_init(&mx, NULL);
 
 	pthread_t process_thread;
 	pthread_t controller_thread;
 
 	pthread_create(&process_thread, NULL, &process, NULL);
 	pthread_create(&controller_thread, NULL, &controller, NULL);
-/*
-	FILE *fp_in = fopen("setpointvalues.txt", "r");
-	FILE *fp_out = fopen("output.txt", "w");
-
-	if (fp_in == NULL) {
-		fprintf(stderr, "Can not open file\n");
-		exit(1);
-	}
-	if (fp_out == NULL) {
-		fprintf(stderr, "Can not write to file\n");
-		exit(1);
-	}
-
-	double y = 0.0;
-	double u = 0.0;
-	double r = 0.0;
-
-	while (fscanf(fp_in, "%lf", &r) != EOF) {
-		y = plant(u);
-		u = pid(r - y);
-		fprintf(fp_out, "%f\n", y);
-	}
-
-	fclose(fp_in);
-	fclose(fp_out);
-*/
-
+	
 	pthread_join(process_thread, NULL);
 	pthread_join(controller_thread, NULL);
+	pthread_mutex_destroy(&mx);
 
 	return EXIT_SUCCESS;
 }
