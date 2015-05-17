f29fb689bb5e470a15ff6d2a44eaf5c663bce192
Abhiram rahatgaonkar <abhrah@student.chalmers.se>
2015-05-17 15:59:03 +0530

    Have a nice control

--- model/main.m
+++ model/main.m
@@ -20,6 +20,11 @@ zeta = [0 0 0]';
 w = [0 0 0]';
 
 % operating point : order - angles_pitch.roll.yaw,angles_dot,translation_x.y.z, translation_dot
-operating_q = [0 0 0 0 0 0 0 0 1 0 0 0]';
+operating_q = [0 0 0 0 0 0 0 0 0.2 0 0 0]';
 operating_u = [1.3 1.3 1.3 1.3]';
+
+[A, B, constant] = linearize(operating_q, operating_u);
+
+Klqr = lqr(A,B,Qx,Qu);
+
 
