b21473630814af5c2bc535aab3dc85df32907f62
Abhiram rahatgaonkar <abhrah@student.chalmers.se>
2015-05-26 23:55:39 +0530

    Modified

--- model/main.m
+++ model/main.m
@@ -4,14 +4,9 @@ clc;
 Ts = 1/100;
 Kv = 14000;
 Qu = diag([5 5 5 5]);
-Qx = 1e-1*diag([10000 10000 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 50]);
+Qx = 1e-1*diag([50 50 10 5 5 5 0.1 0.1 0.1 0.1 0.1 10]);
 m = 0.027;
 g = 9.8;
-
-zeta = [0 0 0]';
-
-% omega = 1000*[1 1 1 1]';
-%w = [0 0 0]';
 
 % operating point : order - angles_pitch.roll.yaw,angles_dot,translation_x.y.z, translation_dot
 operating_q = [0 0 0 0 0 0 0 0 0 0 0 0]';
@@ -24,7 +19,8 @@ Klqr_continuous = lqr(A,B,Qx,Qu);                                               
 Kr_continuous = pinv(D - (C - D*Klqr_continuous)*(A - B*Klqr_continuous)\B);        % continuous for the simulation
 
 sys = ss(A,B,C,D);
-%% Discrete time for real system
+
+% Discrete time for real system
 clc
 sysd = c2d(sys,Ts);
 Ad = sysd.a;
@@ -32,10 +28,11 @@ Bd = sysd.b;
 Cd = sysd.c;
 Dd = sysd.d;
 
-Klqr_d = lqrd(A,B,Qx,Qu,Ts);                % Discrete for real system
+Klqr_d = lqrd(A,B,Qx,Qu,Ts)                % Discrete for real system
 
-Kr_discrete = pinv(Dd - (Cd - Dd*Klqr_d)*(Ad - Bd*Klqr_d)\Bd);
+Kr_discrete = pinv(Dd - (Cd - Dd*Klqr_d)*(Ad - Bd*Klqr_d)\Bd)
 
+eig(Ad - Bd*Klqr_d)                         % eigen values
 %% LQR gain conversion in degrees
 clc
 Klqrd_degrees = [Klqr_d(:,1:6)/(180/pi), Klqr_d(:,7:end)]
@@ -43,7 +40,23 @@ Klqrd_degrees = [Klqr_d(:,1:6)/(180/pi), Klqr_d(:,7:end)]
 Kr_degrees = pinv(Dd - (Cd - Dd*Klqrd_degrees)*(Ad - Bd*Klqrd_degrees)\Bd)
 %initial input in pwm
 
-r = [0 0 0 0 0 0 0 0 0 0 0 0.01]';
-u_initial = (Kr_degrees*(r - operating_q) - Klqrd_degrees*(zeros(12,1) - operating_q) + operating_voltage)*(65535/4)
+r = [0 0 0 0 0 0 0 0 0 0 0 0]';                         % reference
+u_initial = ( Kr_degrees*(r - operating_q) - Klqrd_degrees*(zeros(12,1) - operating_q) + operating_voltage ) %*(65535/4)
 % Kr_degrees*(r - operating_q)
 % Klqrd_degrees*(zeros(12,1)- operating_q)
+
+
+%% Debug
+clc
+x = [0 0 0 0 0 0 0 0 0 0 0 0]';
+klqr = Klqrd_degrees;
+kr = Kr_degrees;
+x_op = operating_q;
+u_op = operating_voltage;
+
+dx = x - x_op;
+dr = r - x_op;
+r_op = kr * dr;
+kdx = klqr * dx;
+du = r_op - kdx;
+u = du + u_op;
